ba7524e6d4c92a9677cf1cc0ce4d971dfbb4ffce
Fidel Castro <fidelcastro4080@gmail.com>
2026-03-25 09:43:31 +0300

    added auth middleware

--- routes/main.js
+++ routes/main.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { login, dashboard } = require('../controllers/main.js');
+const authMiddleware = require('../middlewares/auth.js');
 
-router.post('/login', login);
-router.get('/dashboard', dashboard);
+router.route('/dashboard').get(authMiddleware, dashboard);
+router.route('/login').post(login);
 
 module.exports = router;
