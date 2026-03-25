3c483036b36467ced4aa2f6ee4ef47713bf4d535
Fidel Castro <fidelcastro4080@gmail.com>
2026-03-25 17:43:45 +0300

    auth middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,26 @@
+const jwt = require('jsonwebtoken');
+const CustomAPIError = require('../errors/customError.js');
+
 const authenticationMiddleware = async (req, res, next)=>{
-    console.log(req.headers.authorization);
-    next();
+    const authHeader = req.headers.authorization;
+
+    //Check if token is present
+    if(!authHeader || !authHeader.startsWith('Bearer ')){
+        throw new CustomAPIError('No token Provided!', 401);
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const {id, username} = decoded;
+
+        req.user = {id, username};
+        next();
+    } catch (error) {
+        console.log(error);
+        throw new CustomAPIError('Not authorized to access this route', 401);
+    }
 }
 
 module.exports = authenticationMiddleware;
